982f5bc79867c543e7ce32e6bcc697734a534af2
minalchoudhary1210-pixel <minalchoudhary1210@gmail.com>
2025-11-19 23:12:31 +0530

    CleaningData.sql

--- CleaningData.sql
+++ CleaningData.sql
@@ -1,12 +1,11 @@
 #CLEANING
 
-
-#We only remove rows that are 100% identical (Invoice, StockCode, Description, Quantity, InvoiceDate, Price, CustomerID, Country).
+  
 CREATE TABLE retail_step1_dedup AS
 SELECT DISTINCT *
 FROM online_retail_raw;
 
-#Remove rows with missing Description or invalid Price
+
 CREATE TABLE retail_step2_cleantext AS
 SELECT *
 FROM retail_step1_dedup
@@ -14,25 +13,25 @@ WHERE Description IS NOT NULL
   AND Description <> ''
   AND Price > 0;
   
-#Handle returns (negative quantities)  
+
 CREATE TABLE retail_returns AS
 SELECT *
 FROM retail_step2_cleantext
 WHERE Quantity < 0;
 
-#Create sales-only table
+
 CREATE TABLE retail_step3_sales AS
 SELECT *
 FROM retail_step2_cleantext
 WHERE Quantity > 0;
 
-# Convert text InvoiceDate → DATETIME
+
 CREATE TABLE retail_step4_datetime AS
 SELECT *,
        STR_TO_DATE(InvoiceDate, '%d-%m-%Y %H:%i') AS InvoiceDate_clean
 FROM retail_step3_sales;
 
-#Identify Cancelled Orders
+
 CREATE TABLE retail_step5_cancel_flag AS
 SELECT *,
        CASE 
@@ -41,7 +40,7 @@ SELECT *,
        END AS IsCancelled
 FROM retail_step4_datetime;
 
-# Create the FINAL CLEAN TABLE and remove cancellations and add a TotalPrice column.
+
 CREATE TABLE online_retail_clean AS
 SELECT *,
        (Quantity * Price) AS TotalPrice
